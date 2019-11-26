8259237a88defbfa300357357b8996a28dda4eea
Dave Martin <dave.martin@cxpartners.co.uk>
2019-11-26 15:24:24 +0000

    updating fake page component

--- app/components/FakePage/component.jsx
+++ app/components/FakePage/component.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Container from '../Container/component';
+import Footer from '../Footer/component';
+import Header from '../Header/component';
+import Main from '../Main/component';
+import PhaseBanner from '../PhaseBanner/component';
 
 const FakePage = (props) => {
-  const { src } = props;
+  const { href, src } = props;
 
   return (
-    <div className="gdskit-fakepage">
-      <img src={src} alt="" />
-    </div>
+    <>
+      <Header href="/" />
+      <Container>
+        <PhaseBanner />
+        <Main>
+          <div className="gdskit-fakepage">
+            <img src={src} alt="" />
+          </div>
+        </Main>
+      </Container>
+      <Footer href={href} />
+    </>
   );
 };
 
 export default FakePage;
 
 FakePage.propTypes = {
+  href: PropTypes.string,
   src: PropTypes.string.isRequired,
+};
+
+FakePage.defaultProps = {
+  href: '',
 };
